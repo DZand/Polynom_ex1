c8e8df95c03b8d8adf428cff5afe98efedb8dda8
DZand <48325754+DZand@users.noreply.github.com>
2019-11-14 22:59:29 +0200

    Update PolynomTest.java

--- PolynomTest.java
+++ PolynomTest.java
@@ -6,7 +6,52 @@ public class PolynomTest
 	{
 		//test1();
 		//test2();
+		positiveTests();
+
+	}
+	public static void test1() throws Exception 
+	{
+		Polynom p1 = new Polynom();
+		String[] monoms = {"1","x","x^2", "0.5x^2"};
+		for(int i=0;i<monoms.length;i++) 
+		{
+		Monom m = new Monom(monoms[i]);
+		p1.add(m);
+		double aa = p1.area(0, 1, 0.0001);
+		p1.substract(p1);
+		System.out.println(p1);
+		}
+		//p1.RemoveSamePower(p1);
+		System.out.println(p1);
 		
+	}
+	public static void test2() throws Exception 
+	{
+		Polynom p1 = new Polynom(), p2 =  new Polynom();
+		String[] monoms1 = {"2", "-x","-3.2x^2","4","-1.5x^2"};
+		String[] monoms2 = {"5", "1.7x","3.2x^2","-3","-1.5x^2"};
+		for(int i=0;i<monoms1.length;i++) 
+		{
+			Monom m = new Monom(monoms1[i]);
+			p1.add(m);
+		}
+		for(int i=0;i<monoms2.length;i++) 
+		{
+			Monom m = new Monom(monoms2[i]);
+			p2.add(m);
+		}
+		System.out.println("p1: "+p1);
+		System.out.println("p2: "+p2);
+		//p1.add(p2);
+		//System.out.println("p1+p2: "+p1);
+		p1.multiply(p2);
+		System.out.println("(p1+p2)*p2: "+p1);
+		String s1 = p1.toString();
+		//Polynom_able pp1 = Polynom.parse(s1);
+		//System.out.println("from string: "+pp1);
+	}
+	public static void positiveTests() throws Exception
+	{
 		Polynom p1 = new Polynom("x^2-2x+1");
 		Polynom p3 = new Polynom();
 		Monom m1 = new Monom("x^3");
@@ -71,52 +116,6 @@ public class PolynomTest
 		System.out.println("The ans should be : 80.9999 --> " + p15.area(-6, 3, 0.001));
 		
 		Polynom p16= new Polynom("y^3");
+	}
 
-
-	}
-	public static void test1() throws Exception 
-	{
-		Polynom p1 = new Polynom();
-		String[] monoms = {"1","x","x^2", "0.5x^2"};
-		for(int i=0;i<monoms.length;i++) 
-		{
-		Monom m = new Monom(monoms[i]);
-		p1.add(m);
-		double aa = p1.area(0, 1, 0.0001);
-		p1.substract(p1);
-		System.out.println(p1);
-		}
-		//p1.RemoveSamePower(p1);
-		System.out.println(p1);
-		
-	}
-	public static void test2() throws Exception 
-	{
-		Polynom p1 = new Polynom(), p2 =  new Polynom();
-		String[] monoms1 = {"2", "-x","-3.2x^2","4","-1.5x^2"};
-		String[] monoms2 = {"5", "1.7x","3.2x^2","-3","-1.5x^2"};
-		for(int i=0;i<monoms1.length;i++) 
-		{
-			Monom m = new Monom(monoms1[i]);
-			p1.add(m);
-		}
-		for(int i=0;i<monoms2.length;i++) 
-		{
-			Monom m = new Monom(monoms2[i]);
-			p2.add(m);
-		}
-		System.out.println("p1: "+p1);
-		System.out.println("p2: "+p2);
-		//p1.add(p2);
-		//System.out.println("p1+p2: "+p1);
-		p1.multiply(p2);
-		System.out.println("(p1+p2)*p2: "+p1);
-		String s1 = p1.toString();
-		//Polynom_able pp1 = Polynom.parse(s1);
-		//System.out.println("from string: "+pp1);
-	}
-	public static void test3() throws Exception
-	{
-		
-	}
 }
